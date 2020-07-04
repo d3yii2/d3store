dbbd489a9598980af6a4908cd0fd9cc10e0ea7df
uldisn <uldisnelsons@gmail.com>
2020-07-04 14:19:55 +0300

    StackDictionary improvment

--- dictionaries/StackDictionary.php
+++ dictionaries/StackDictionary.php
@@ -10,6 +10,7 @@ use yii\helpers\ArrayHelper;
 class StackDictionary{
 
     private const CACHE_KEY_LIST = 'StackDictionaryList';
+    private const CACHE_KEY_LIST_STACK_NAME = 'StackDictionaryListStName';
 
     public static function getList(int $storeId = 0,bool $full = true): array
     {
@@ -29,6 +30,34 @@ class StackDictionary{
                     ->select([
                         'store_stack.id',
                         'concat(store.name, " - ", store_stack.name) name'
+                    ])
+                    ->innerJoin('store_store store','store.id = store_stack.store_id')
+                    ->where($conditions)
+                    ->orderBy('store.name, store_stack.name')
+                    ->all();
+                return ArrayHelper::map($stocks, 'id', 'name');
+            }
+        );
+    }
+
+    public static function getStackNameList(int $storeId = 0,bool $full = true): array
+    {
+        return Yii::$app->cache->getOrSet(
+            self::createKeyStackNameList($storeId, $full),
+            static function () use($storeId, $full) {
+                $conditions = [];
+                if($storeId) {
+                    $conditions = [
+                        'store_id' => $storeId
+                    ];
+                    if (!$full) {
+                        $conditions['store_stack.active'] = 1;
+                    }
+                }
+                $stocks = StoreStack::find()
+                    ->select([
+                        'store_stack.id',
+                        'store_stack.name name'
                     ])
                     ->innerJoin('store_store store','store.id = store_stack.store_id')
                     ->where($conditions)
@@ -68,6 +97,10 @@ class StackDictionary{
     {
         return self::CACHE_KEY_LIST . '-LIST-' . $storeId . '-' . ($full?'TRUE':'FALSE');
     }
+    private static function createKeyStackNameList(int $storeId, bool $full): string
+    {
+        return self::CACHE_KEY_LIST_STACK_NAME . '-LIST-' . $storeId . '-' . ($full?'TRUE':'FALSE');
+    }
 
     private static function createKeyCompany(array $companyIdList,bool $full): string
     {
@@ -86,6 +119,10 @@ class StackDictionary{
             Yii::$app->cache->delete(self::createKeyList(0, true));
             Yii::$app->cache->delete(self::createKeyList($store->id, false));
             Yii::$app->cache->delete(self::createKeyList($store->id, true));
+            Yii::$app->cache->delete(self::createKeyStackNameList(0, false));
+            Yii::$app->cache->delete(self::createKeyStackNameList(0, true));
+            Yii::$app->cache->delete(self::createKeyStackNameList($store->id, false));
+            Yii::$app->cache->delete(self::createKeyStackNameList($store->id, true));
 
         }
         foreach(self::getCompanyKeyList() as $key){
