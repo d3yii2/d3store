c9b3ab774518cba1f0b14e721ab55ef808bd7707
uldis <uldis@weberp.lv>
2024-11-29 14:01:09 +0200

    fixes

--- messages/lv/d3store.php
+++ messages/lv/d3store.php
@@ -1,3 +1,5 @@
 <?php
 
-return [];
+return [
+    'New fix quantity cannot be negative' => "Jaunā labojuma daudzums nevar būt negatīvs"
+];
--- repository/Fix.php
+++ repository/Fix.php
@@ -7,7 +7,10 @@ namespace d3yii2\d3store\repository;
 use d3system\exceptions\D3ActiveRecordException;
 use d3yii2\d3store\models\StoreFixes;
 use d3yii2\d3store\models\StoreTransactions;
+use Yii;
+use yii\base\UserException;
 use yii\db\ActiveRecord;
+use yii\db\Exception;
 
 class Fix
 {
@@ -29,9 +32,13 @@ class Fix
      * @param null|int $userId
      * @param null|ActiveRecord|object $refModel
      * @throws D3ActiveRecordException
+     * @throws UserException|Exception
      */
-    public function register(float $qnt, $userId = null, $refModel = null): void
+    public function register(float $qnt, int $userId = null, $refModel = null): void
     {
+        if ($qnt < 0) {
+            throw new UserException(Yii::t('d3store','New fix quantity cannot be negative'));
+        }
         $fixQnt = $qnt - $this->transaction->remain_quantity;
         $this->transaction->remain_quantity = $qnt;
         $fixModel = new StoreFixes();
