c20380ed2e1e14306b820891a5a62d45a6065b2c
uldis <uldis@weberp.lv>
2025-08-20 13:25:50 +0300

    Enhance `StoreTransactions` model with quantity validation adjustments and `createTranAdd` method for alternative quantity handling.

--- models/StoreTransactions.php
+++ models/StoreTransactions.php
@@ -2,10 +2,12 @@
 
 namespace d3yii2\d3store\models;
 
-use \d3yii2\d3store\models\base\StoreTransactions as BaseStoreTransactions;
+use d3system\exceptions\D3ActiveRecordException;
+use d3yii2\d3store\models\base\StoreTransactions as BaseStoreTransactions;
 use d3yii2\d3store\repository\Transactions;
 use DateTime;
 use Yii;
+use yii\db\Exception;
 
 /**
  * This is the model class for table "store_transactions".
@@ -25,21 +27,22 @@ class StoreTransactions extends BaseStoreTransactions
             parent::rules(),
             [
                 ['quantity', 'validateQuantity'],
-            ]);
+            ]
+        );
     }
 
     public function validateQuantity($attribute): void
     {
-        if($this->isNewRecord) {
+        if ($this->isNewRecord) {
             if (self::ACTION_MOVE === $this->action || self::ACTION_UNLOAD === $this->action) {
                 $balance = Transactions::getStackBalance($this->stack_from);
-                if (round($balance,3) < round($this->$attribute,3)) {
+                if (round($balance, 3) < round($this->$attribute, 3)) {
                     $error = Yii::t(
                         'd3store',
                         'Stacks actual balance {stockBalance} is smallest requested {quantity} quantity',
                         [
-                            'stockBalance' => round($balance,3),
-                            'quantity' => round($this->$attribute,3),
+                            'stockBalance' => round($balance, 3),
+                            'quantity' => round($this->$attribute, 3),
                         ]
                     );
                     $this->addError($attribute, $error);
@@ -48,9 +51,31 @@ class StoreTransactions extends BaseStoreTransactions
         }
     }
 
+    /**
+     * @throws \yii\base\Exception
+     * @throws \Exception
+     */
     public function createMove(): bool
     {
         return (bool)Transactions::moveFifo(new DateTime($this->tran_time), $this->quantity, $this->stack_from, $this->stack_to);
     }
 
+    /**
+     * save alternative quantity
+     * @throws Exception
+     * @throws D3ActiveRecordException
+     */
+    public function createTranAdd(int $typeId, float $qnt): StoreTranAdd
+    {
+        $tranAdd = new StoreTranAdd([
+            'type_id' => $typeId,
+            'transactions_id' => $this->id,
+            'quantity' => $qnt,
+            'remain_quantity' => $qnt,
+        ]);
+        if (!$tranAdd->save()) {
+            throw new D3ActiveRecordException($tranAdd);
+        }
+        return $tranAdd;
+    }
 }
